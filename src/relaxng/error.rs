f7b24138bbb50cea3e3a3408fd5674f33152ddd5
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2022-10-24 08:43:56 +0100

    Extract parser helpers

--- src/relaxng/error.rs
+++ src/relaxng/error.rs
@@ -1,9 +1,14 @@
 use thiserror::Error;
 
+use super::parser::Span;
+
 #[derive(Debug, Error)]
 pub enum RelaxNgError {
     #[error("invalid url")]
     InvalidUrl(#[from] url::ParseError),
+
+    #[error(transparent)]
+    ParseError(#[from] nom::error::Error<String>),
 }
 
 pub type RelaxNgResult<T> = Result<T, RelaxNgError>;
--- src/relaxng/parser/mod.rs
+++ src/relaxng/parser/mod.rs
@@ -1,64 +1,25 @@
 #[cfg(test)]
 mod tests;
 
-use nom::branch::alt;
-use nom::bytes::complete::{escaped, take, take_until};
-use nom::character::complete::line_ending;
-use nom::character::complete::{multispace0, multispace1, one_of};
-use nom::combinator::{opt, value};
+mod helpers;
+
+use nom::character::complete::{multispace0, multispace1};
+
 use nom::multi::{many0, many_m_n};
-use nom::sequence::{pair, preceded, terminated, tuple};
+
 use nom::{
-    bytes::complete::tag,
-    bytes::complete::tag_no_case,
-    character::complete::{alphanumeric1, char},
-    combinator::map_res,
-    sequence::{delimited, separated_pair},
-    IResult,
+    bytes::complete::tag, bytes::complete::tag_no_case, character::complete::alphanumeric1,
+    combinator::map_res, sequence::separated_pair, IResult,
 };
 
 use nom_locate::LocatedSpan;
 
-use super::error::RelaxNgError;
+use self::helpers::{quoted, skip_comment_lines, ws};
+
+use super::error::{RelaxNgError, RelaxNgResult};
 use super::Namespace;
 
 pub type Span<'a> = LocatedSpan<&'a str>;
-
-/// A combinator that takes a parser `inner` and produces a parser that also consumes both leading and
-/// trailing whitespace, returning the output of `inner`.
-fn ws<'a, F, O, E>(inner: F) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, O, E>
-where
-    F: Fn(Span<'a>) -> IResult<Span<'a>, O, E>,
-    E: nom::error::ParseError<Span<'a>>,
-{
-    delimited(multispace0, inner, multispace0)
-}
-
-fn quoted(input: Span) -> IResult<Span, Span> {
-    delimited(
-        char('"'),
-        escaped(take_until("\""), '\\', one_of(r#""n\"#)),
-        char('"'),
-    )(input)
-}
-
-fn comment(input: Span) -> IResult<Span, ()> {
-    value((), tuple((ws(tag("#")), take_until("\n"), take(1usize))))(input)
-}
-
-fn skip_comments_eol<'a, F, O>(inner: F) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, O>
-where
-    F: Fn(Span<'a>) -> IResult<Span<'a>, O>,
-{
-    terminated(ws(inner), opt(comment))
-}
-
-fn skip_comment_lines<'a, F, O>(inner: F) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, O>
-where
-    F: Fn(Span<'a>) -> IResult<Span<'a>, O>,
-{
-    preceded(many0(comment), ws(inner))
-}
 
 fn parse_namespace(input: Span) -> IResult<Span, (bool, Namespace)> {
     let (input, is_default) = map_res(many_m_n(0, 1, tag("default")), |d| {
@@ -78,8 +39,12 @@ fn parse_namespaces(input: Span) -> IResult<Span, Vec<(bool, Namespace)>> {
     many0(skip_comment_lines(parse_namespace))(input)
 }
 
-pub fn parse(s: &str) {
-    let span = Span::new(s);
+pub fn parse(s: &str) -> RelaxNgResult<()> {
+    let input = Span::new(s);
 
     // SchemaBuilder::default().build();
+
+    let (input, ns) = parse_namespaces(input);
+
+    Ok(())
 }
